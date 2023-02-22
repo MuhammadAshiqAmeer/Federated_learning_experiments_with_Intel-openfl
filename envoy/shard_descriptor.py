4ee4f2fc5cc49eb85ab6dfe891a8ed230b8d5575
Muhammad Ashiq Ameer <muhammadashiqameer@gmail.com>
2023-02-22 12:11:08 +0530

    feature mapping

--- envoy/shard_descriptor.py
+++ envoy/shard_descriptor.py
@@ -12,6 +12,8 @@ It is recommended to perform tensor manipulations using numpy.
 """Mnist Shard Descriptor."""
 from typing import List, Tuple
 import logging
+import pandas as pd
+import json
 import os
 from typing import List
 import tensorflow as tf
@@ -25,11 +27,9 @@ logger = logging.getLogger(__name__)
 class neuroblastomaShardDataset(ShardDataset):
     """Lem-Mel Shard dataset class."""
 
-    def __init__(self, x, y, data_type, rank=1, worldsize=1):
+    def __init__(self, x, y, data_type):
         """Pick rank-specific subset of (x, y)"""
         self.data_type = data_type
-        self.rank = rank
-        self.worldsize = worldsize
         self.x=x
         self.y=y
         #self.x = x[self.rank - 1::self.worldsize]
@@ -46,14 +46,8 @@ class neuroblastomaShardDataset(ShardDataset):
 class neuroblastomaShardDescriptor(ShardDescriptor):
     """Lem-Mel Shard descriptor class."""
 
-    def __init__(
-            self,
-            rank_worldsize: str = '1, 1',
-            **kwargs
-    ):
-        """Initialize Lem-Mel ShardDescriptor."""
-        self.rank, self.worldsize = tuple(int(num) for num in rank_worldsize.split(','))
-        (x_train, y_train), (x_test, y_test) = self.load_prepare_data(rank=self.rank, worldsize=self.worldsize)
+    def __init__(self):
+        (x_train, y_train), (x_test, y_test) = self.load_prepare_data()
 
         self.data_by_type = {
             'train': (x_train, y_train),
@@ -71,9 +65,7 @@ class neuroblastomaShardDescriptor(ShardDescriptor):
             raise Exception(f'Wrong dataset type: {dataset_type}')
         return neuroblastomaShardDataset(
             *self.data_by_type[dataset_type],
-            data_type=dataset_type,
-            rank=self.rank,
-            worldsize=self.worldsize
+            data_type=dataset_type
         )
 
     @property
@@ -86,22 +78,34 @@ class neuroblastomaShardDescriptor(ShardDescriptor):
         """Return the target shape info."""
         return ['1']
 
-    @property
-    def dataset_description(self) -> str:
-        """Return the dataset description."""
-        return (f'Dataset number {self.rank}'
-                f' out of {self.worldsize}')
     @staticmethod
-    def load_prepare_data(rank: int, worldsize: int) -> Tuple[tf.data.Dataset]:
+    def load_prepare_data() -> Tuple[tf.data.Dataset]:
         """Load and prepare dataset."""
 
+        # Opening JSON file
+        f = open('map.json')
+  
+        # returns JSON object as 
+        # a dictionary
+        data = json.load(f)
+  
+        # Iterating through the json
+        list1=[]
+        mappings="{"
+        for i in data['mapping']:
+                   mylocal=json.dumps(i['local']).strip('"')
+                   myglobal=json.dumps(i['global']).strip('"')
+                   list1+=[mylocal]
+                   mappings+='"'+mylocal+'":"'+myglobal+'",'
+      
+        mappings=mappings[:-1]
+        mappings+="}"
 
-        X = []
-        y = []
+        df = pd.read_csv("train_t.csv",header=0,usecols=list1) 
+        df.rename(columns=json.loads(mappings),inplace=True)
+        f.close()
         
-        import pandas as pd
-        df = pd.read_excel("train_2.xlsx")
-        
+        # Preprocessing
         df["Gender"]=df["Gender"].replace({"Male":1,"Female":0})
         df["Vital Status"]=df["Vital Status"].replace({"Dead":0,"Alive":1})
         df["INSS Stage"]=df["INSS Stage"].replace({"Stage 1":0,"Stage 2a":1,"Stage 2b":2,"Stage 3":3,"Stage 4":4,"Stage 4s":5,})
@@ -110,10 +114,15 @@ class neuroblastomaShardDescriptor(ShardDescriptor):
         df["MKI"]=df["MKI"].replace({"Low":0,"High":1,"Intermediate":2})
         df["COG Risk Group"]=df["COG Risk Group"].replace({"Low Risk":0,"Intermediate Risk":1,"High Risk":2})
         df["Overall Survival Time in Years"] = round(df["Overall Survival Time in Days"]/365,0)
-        x=df.drop(['Vital Status','TARGET USI','Overall Survival Time in Days','Gender'],axis=1)
+        df["Age at Diagnosis in years"] = round(df["Age at Diagnosis in Days"]/365,0)
+        df["Event Free Survival Time in years"] = round(df["Event Free Survival Time in Days"]/365,0)
+        x=df.drop(['Vital Status','TARGET USI','Overall Survival Time in Days','Gender','Event Free Survival Time in Days','Age at Diagnosis in Days'],axis=1)
         y=df['Vital Status']
 
-        df_t = pd.read_excel("test_2.xlsx")
+        df_t= pd.read_csv("test_t.csv",header=0,usecols=list1) 
+        df_t.rename(columns=json.loads(mappings),inplace=True)
+        f.close()
+
         df_t["Vital Status"]=df_t["Vital Status"].replace({"Dead":0,"Alive":1})
         df_t["INSS Stage"]=df_t["INSS Stage"].replace({"Stage 1":0,"Stage 2a":1,"Stage 2b":2,"Stage 3":3,"Stage 4":4,"Stage 4s":5,})
         df_t["MYCN status"]=df_t["MYCN status"].replace({"Not Amplified":0,"Amplified":1})
@@ -122,7 +131,9 @@ class neuroblastomaShardDescriptor(ShardDescriptor):
         df_t["MKI"]=df_t["MKI"].replace({"Low":0,"High":1,"Intermediate":2})
         df_t["COG Risk Group"]=df_t["COG Risk Group"].replace({"Low Risk":0,"Intermediate Risk":1,"High Risk":2})
         df_t["Overall Survival Time in Years"] = round(df_t["Overall Survival Time in Days"]/365,0)
-        x_t=df_t.drop(['Vital Status','TARGET USI','Overall Survival Time in Days','Gender'],axis=1)
+        df_t["Age at Diagnosis in years"] = round(df_t["Age at Diagnosis in Days"]/365,0)
+        df_t["Event Free Survival Time in years"] = round(df_t["Event Free Survival Time in Days"]/365,0)
+        x_t=df_t.drop(['Vital Status','TARGET USI','Overall Survival Time in Days','Gender','Event Free Survival Time in Days','Age at Diagnosis in Days'],axis=1)
         y_t=df_t['Vital Status']
 
         from sklearn.preprocessing import StandardScaler
@@ -132,9 +143,10 @@ class neuroblastomaShardDescriptor(ShardDescriptor):
         test_scaled = scaler.fit_transform(x_t)
        
         from sklearn.model_selection import train_test_split
-        x_train, x_test, y_train, y_test = np.array(train_scaled), np.array(test_scaled), np.array(y), np.array(y_t)
+        x_train, x_test, y_train, y_test = train_scaled, test_scaled, y, y_t
         y_train=np.asarray(y_train).reshape((-1,1))
         y_test=np.asarray(y_test).reshape((-1,1))
+   
    
         print('Data was loaded!')
         return (x_train, y_train), (x_test, y_test)
